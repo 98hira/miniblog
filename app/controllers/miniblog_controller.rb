08f9e80bbd7fe87406f423ef21cffa3cba74a9b8
98hira <flattail2018@gmail.com>
2019-02-18 07:26:07 +0900

    [add]記事作成に関する処理の実装
    以下の対応を行った。
    ・記事作成画面へのリンク貼り
    ・記事作成時のメッセージの表示
    ・記事保存時にテキストがカラの時は失敗するようにバリデーション追加
    
    [time]2019/02/18-07:28

--- app/controllers/miniblog_controller.rb
+++ app/controllers/miniblog_controller.rb
@@ -9,10 +9,16 @@ class MiniblogController < ApplicationController
   def create
     tweet = tweet_params.merge(user_id: current_user.id)
     success_flag = Tweet.create(tweet).valid?
+    @status = success_flag ? "成功": "失敗"
+  end
+
+  def edit
+    @tweet = Tweet.find(params.permit[:id])
   end
 
   private
   def tweet_params
    params.permit(:text)
  end
+
 end
