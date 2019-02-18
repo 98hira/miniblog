770ef216cba3da23d82ed7b438932f552a4a0474
98hira <flattail2018@gmail.com>
2019-02-18 13:52:41 +0900

    [add]記事へのコメント管理用のテーブル作成しアソシエーションを設定
    [time]2019/02/18-13:53

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
-class Tweet < ApplicationRecord
+class Comment < ApplicationRecord
+  belongs_to :tweet
   belongs_to :user
   validates :text, presence: true
-  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_17_113127) do
+ActiveRecord::Schema.define(version: 2019_02_18_042205) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text"
